3d43de44d38264d1c5e6c00be52a0629f5b2a81a
RxN <120871604+Ricofzm@users.noreply.github.com>
2026-05-22 13:38:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,100 +1,6 @@
-/* SCENE */
-let scene =
-new THREE.Scene();
-
-scene.background =
-new THREE.Color(0x111111);
-
-/* CAMERA */
-let camera =
-new THREE.PerspectiveCamera(
-75,
-window.innerWidth/window.innerHeight,
-0.1,
-1000
-);
-
-camera.position.set(0,1.5,3);
-
-/* RENDERER */
-let renderer =
-new THREE.WebGLRenderer({
-antialias:true
-});
-
-renderer.setSize(
-window.innerWidth,
-window.innerHeight
-);
-
-renderer.setPixelRatio(
-window.devicePixelRatio * 0.7
-);
-
-document
-.getElementById("viewer")
-.appendChild(renderer.domElement);
-
-/* CONTROLS */
-let controls =
-new THREE.OrbitControls(
-camera,
-renderer.domElement
-);
-
-controls.enableDamping = true;
-
-/* LIGHT */
-let hemiLight =
-new THREE.HemisphereLight(
-0xffffff,
-0x444444,
-3
-);
-
-scene.add(hemiLight);
-
-let dirLight =
-new THREE.DirectionalLight(
-0xffffff,
-2
-);
-
-dirLight.position.set(5,10,5);
-
-scene.add(dirLight);
-
-/* FLOOR */
-let floorGeometry =
-new THREE.CircleGeometry(5,64);
-
-let floorMaterial =
-new THREE.MeshStandardMaterial({
-color:"#222"
-});
-
-let floor =
-new THREE.Mesh(
-floorGeometry,
-floorMaterial
-);
-
-floor.rotation.x =
--Math.PI/2;
-
-floor.position.y =
--1;
-
-scene.add(floor);
-
-/* LOADER */
-let loader =
-new THREE.GLTFLoader();
-
-/* LOAD MODEL */
 loader.load(
 
-'models/body.glb',
+'./models/body.glb',
 
 function(gltf){
 
@@ -113,14 +19,9 @@ function(gltf){
     0
   );
 
-  model.rotation.y =
-  Math.PI;
-
   scene.add(model);
 
-  console.log(
-    "MODEL BERHASIL"
-  );
+  console.log("MODEL MASUK");
 
 },
 
@@ -133,40 +34,3 @@ function(error){
 }
 
 );
-
-/* ANIMATION */
-function animate(){
-
-  requestAnimationFrame(
-    animate
-  );
-
-  controls.update();
-
-  renderer.render(
-    scene,
-    camera
-  );
-
-}
-
-animate();
-
-/* RESPONSIVE */
-window.addEventListener(
-'resize',
-
-function(){
-
-  camera.aspect =
-  window.innerWidth /
-  window.innerHeight;
-
-  camera.updateProjectionMatrix();
-
-  renderer.setSize(
-    window.innerWidth,
-    window.innerHeight
-  );
-
-});
