0cb06be74c62c51c68c5b2dc2d4d7d6a805f629f
RxN <120871604+Ricofzm@users.noreply.github.com>
2026-05-23 16:41:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,39 @@ function changeShoes(img){
 document.getElementById("shoesLayer").src = img;
 
 }
+
+/* CATEGORY */
+
+function showCategory(category,element){
+
+// hide all
+
+document
+.querySelectorAll(".category")
+.forEach(el=>{
+
+el.style.display="none";
+
+});
+
+// show selected
+
+document
+.getElementById(category)
+.style.display="grid";
+
+// remove active tab
+
+document
+.querySelectorAll(".tab")
+.forEach(tab=>{
+
+tab.classList.remove("active");
+
+});
+
+// add active tab
+
+element.classList.add("active");
+
+}
