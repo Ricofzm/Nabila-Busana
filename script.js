896fd16c97a4e13920ee941abcac63e42d0158c2
RxN <120871604+Ricofzm@users.noreply.github.com>
2026-05-23 16:53:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -51,3 +51,69 @@ tab.classList.remove("active");
 element.classList.add("active");
 
 }
+
+/* ZOOM ROTATE */
+
+let scale = 1;
+let rotate = 0;
+
+function updateTransform(){
+
+document
+.getElementById("viewerModel")
+.style.transform =
+
+`scale(${scale}) rotate(${rotate}deg)`;
+
+}
+
+/* ZOOM IN */
+
+function zoomIn(){
+
+scale += 0.1;
+
+updateTransform();
+
+}
+
+/* ZOOM OUT */
+
+function zoomOut(){
+
+scale -= 0.1;
+
+updateTransform();
+
+}
+
+/* ROTATE LEFT */
+
+function rotateLeft(){
+
+rotate -= 10;
+
+updateTransform();
+
+}
+
+/* ROTATE RIGHT */
+
+function rotateRight(){
+
+rotate += 10;
+
+updateTransform();
+
+}
+
+/* RESET */
+
+function resetView(){
+
+scale = 1;
+rotate = 0;
+
+updateTransform();
+
+}
