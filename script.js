b4b40c0cde53cdf23397118bfcaf6579e7c962d0
RxN <120871604+Ricofzm@users.noreply.github.com>
2026-05-23 15:49:13 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function changeTop(img){
+
+document.getElementById("topLayer").src = img;
+
+}
+
+function changePants(img){
+
+document.getElementById("pantsLayer").src = img;
+
+}
+
+function changeShoes(img){
+
+document.getElementById("shoesLayer").src = img;
+
+}
