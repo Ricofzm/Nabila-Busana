ea9828bd159e0648fd10faecd6cd369832039bd6
RxN <120871604+Ricofzm@users.noreply.github.com>
2026-05-23 20:49:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -144,9 +144,13 @@ link.click();
 
 }
 
-/* CART */
+/* ===================== */
+/* CART SYSTEM */
+/* ===================== */
 
 let total = 0;
+
+/* ADD TO CART */
 
 function addToCart(
 name,
@@ -158,6 +162,174 @@ const cart =
 document.getElementById(
 "cartItems"
 );
+
+/* ITEM */
+
+const item =
+document.createElement("div");
+
+item.className =
+"cart-item";
+
+/* DEFAULT QTY */
+
+let qty = 1;
+
+/* HTML */
+
+item.innerHTML = `
+
+<img src="${image}">
+
+<div class="cart-info">
+
+<h4>${name}</h4>
+
+<p>
+IDR ${price.toLocaleString()}
+</p>
+
+<div class="qty-controls">
+
+<button onclick="
+changeQty(
+this,
+-${price}
+)">
+−
+</button>
+
+<span class="qty">
+1
+</span>
+
+<button onclick="
+changeQty(
+this,
+${price}
+)">
++
+</button>
+
+</div>
+
+<button
+class="remove-btn"
+onclick="
+removeItem(
+this,
+${price}
+)">
+Remove
+</button>
+
+</div>
+
+`;
+
+/* APPEND */
+
+cart.appendChild(item);
+
+/* TOTAL */
+
+total += price;
+
+updateTotal();
+
+}
+
+/* UPDATE TOTAL */
+
+function updateTotal(){
+
+document.getElementById(
+"totalPrice"
+).innerText =
+
+`IDR ${total.toLocaleString()}`;
+
+}
+
+/* CHANGE QTY */
+
+function changeQty(
+button,
+priceChange
+){
+
+const controls =
+button.parentElement;
+
+const qtyText =
+controls.querySelector(".qty");
+
+let qty =
+parseInt(qtyText.innerText);
+
+/* MINUS */
+
+if(priceChange < 0){
+
+if(qty > 1){
+
+qty--;
+
+total += priceChange;
+
+}
+
+}
+
+/* PLUS */
+
+else{
+
+qty++;
+
+total += priceChange;
+
+}
+
+/* UPDATE */
+
+qtyText.innerText = qty;
+
+updateTotal();
+
+}
+
+/* REMOVE */
+
+function removeItem(
+button,
+price
+){
+
+const item =
+button.closest(".cart-item");
+
+/* GET QTY */
+
+const qty =
+parseInt(
+
+item.querySelector(".qty")
+.innerText
+
+);
+
+/* REMOVE TOTAL */
+
+total -= price * qty;
+
+updateTotal();
+
+/* DELETE */
+
+item.remove();
+
+}
 
 /* ITEM */
 
