ba538429e169bb33ec9a50f880403a9cf45d49cc
RxN <120871604+Ricofzm@users.noreply.github.com>
2026-05-28 04:25:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ type:"pants"
 {
 name:"Sneakers White",
 price:399000,
-thumb:"assets/tbshoes.PNG",
-fit:"assets/shoes-white.png",
+thumb:"assets/tbshoes.png",
+fit:"assets/shoes-white.PNG",
 type:"shoes"
 }
 
